aec7925a51151ec7874f52021015dd45f5dc6f79
Shubham Giri <shubhammgiri3@gmail.com>
2023-12-20 15:34:18 +0530

    2015 day3 modified

--- 2015/day3/index.js
+++ 2015/day3/index.js
@@ -2,21 +2,18 @@ import fs from 'fs';
 
 const INPUT = fs.readFileSync('./input.txt', 'utf-8').trim().split("");
 
-// const INPUT = "^v".split("");
-
 // part one
-
 function countHouse(input) {
     const coordinates = new Set().add(`0x0`);
-    input.reduce((curCoords, direction) => {
-        let newCoords = {x: 0, y: 0};
-        if (direction === '^') newCoords = {x: curCoords.x, y: curCoords.y + 1};
-        if (direction === 'v') newCoords = {x: curCoords.x, y: curCoords.y - 1};
-        if (direction === '>') newCoords = {x: curCoords.x + 1, y: curCoords.y};
-        if (direction === '<') newCoords = {x: curCoords.x - 1, y: curCoords.y};
-        coordinates.add(`${newCoords.x}x${newCoords.y}`);
+    input.reduce((curCoords, dir) => {
+        let newCords = {x: 0, y: 0};
+        if (dir === '^') newCords = {x: curCoords.x, y: curCoords.y + 1};
+        if (dir === 'v') newCords = {x: curCoords.x, y: curCoords.y - 1};
+        if (dir === '>') newCords = {x: curCoords.x + 1, y: curCoords.y};
+        if (dir === '<') newCords = {x: curCoords.x - 1, y: curCoords.y};
+        coordinates.add(`${newCords.x}x${newCords.y}`);
 
-        return newCoords;
+        return newCords;
 
     }, {x: 0, y: 0});
 
@@ -29,15 +26,15 @@ console.log(countHouse(INPUT).size);
 const santaInput = INPUT.filter((_, i) =>  i%2 === 0);
 const roboInput = INPUT.filter((_, i) =>  i%2 !== 0);
 
-function traverse(input) {
+function countHouseAlternate(input) {
   let visitedCords = ['0x0'];
   let curPos = {x: 0, y: 0};
 
-  input.forEach(direction => {
-    if (direction === '^') curPos.y++;
-    if (direction === 'v') curPos.y--;
-    if (direction === '>') curPos.x++;
-    if (direction === '<') curPos.x--;
+  input.forEach(dir => {
+    if (dir === '^') curPos.y++;
+    if (dir === 'v') curPos.y--;
+    if (dir === '>') curPos.x++;
+    if (dir === '<') curPos.x--;
 
     visitedCords.push(`${curPos.x}x${curPos.y}`);
   });
@@ -45,7 +42,7 @@ function traverse(input) {
   return visitedCords;
 };
 
-const result = new Set(traverse(santaInput).concat(traverse(roboInput))).size;
+const result = new Set(countHouseAlternate(santaInput).concat(countHouseAlternate(roboInput))).size;
 
 console.log(result);
 
